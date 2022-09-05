00b52d34fa0efc913608658d7d0ce006b3bc5179
WanFaiq <39939596+WanFaiq@users.noreply.github.com>
2022-09-05 14:41:26 +0800

    Add files via upload
    Raylib in C and CPP

---
+++ Raylib test/game.cpp
@@ -0,0 +1,15 @@
+#include "raylib.h"
+int main()
+{
+    int width;
+    width = 350;
+    InitWindow (width, 200, "Scammer's corner");
+
+    while (true )
+    {
+       BeginDrawing();
+       ClearBackground(RED);
+       EndDrawing();
+
+    }
+}
